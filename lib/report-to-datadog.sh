61d3979e23d71dec9a05af7a6d0fb0325a986d8e
andy.mackinlay <andy.mackinlay@cultureamp.com>
2020-12-14 08:46:29 +1100

    check for presence of `nc` before deciding to use docker version of netcat

--- lib/report-to-datadog.sh
+++ lib/report-to-datadog.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# try to use native netcat where it exists as sometimes the docker version doesn't have 
+# permissions to do host networking. If you have disabled user namespace remapping and don't 
+# have `nc` installed this will still silently fail
+NC=nc
+if ! which -s "$NC" ; then 
+  NC="docker run -i --rm --network=host subfuzion/netcat"
+fi
+
 function reportToDatadog() {
   local metric_name=$1
   local metric_value=$2
@@ -10,6 +18,6 @@ function reportToDatadog() {
   DD_PORT=${BUILDKITE_PLUGIN_DATADOG_STATS_DOGSTATSD_PORT:-8125}
 
   echo "Reporting ${metric_name} with value=${metric_value}, type=${metric_type}, tags=${tags}"
-  echo "Sending to ${DD_HOST}:${DD_PORT}..."
-  echo -n "${metric_name}:${metric_value}|${metric_type}|#${tags}" | docker run -i --rm --network=host subfuzion/netcat -4u -w1 "${DD_HOST}" "${DD_PORT}"
+  echo "Sending to ${DD_HOST}:${DD_PORT} using command '${NC}'..."
+  echo -n "${metric_name}:${metric_value}|${metric_type}|#${tags}" | $NC "${DD_HOST}" "${DD_PORT}"
 }
